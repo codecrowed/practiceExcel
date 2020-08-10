f9160ae6bfbecf6c1877dc542bbc3c4e86a974e8
jiangyangang <jiangyangang@yunshugroup.com>
2020-08-10 20:33:19 +0800

    Initial commit

---
+++ src/main/java/com/jyg/procatice/ProcaticeApplication.java
@@ -0,0 +1,13 @@
+package com.jyg.procatice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcaticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcaticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jyg/procatice/ProcaticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyg.procatice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcaticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
